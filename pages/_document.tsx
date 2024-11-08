310c60fe004a108a0872a862d1fc9e0dbb6d1e88
mumar9504 <minu0709ms@gmail.com>
2024-11-09 05:58:22 +0900

    feat: meta tegs integrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log("DOCUMENT - PAGES ROUTER");
-  
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Core Mattress Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/coreMattress_favicon.svg" />
+
+        {/* SEO */}
+        <meta
+          name="keyword"
+          content={"core mattress, core mattress.uz, mern nestjs fullstack"}
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere anytime in South Korea. | " +
+            "Покупайте и продавайте недвижимость в любом месте и в любое время в Южной Корее. | " +
+            "대한민국 어디서나 언제든지 메트리스를 사고 팔 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
